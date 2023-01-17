ff7b1698148e8f576ef7c3814d15701cd1fde524
MaxJackson13 <72030655+MaxJackson13@users.noreply.github.com>
2023-01-17 14:12:35 +0000

    Update health_check.pl

--- health_check.pl
+++ health_check.pl
@@ -3,7 +3,7 @@ use IO::Socket::SSL;
 
 my $ua = LWP::UserAgent->new;
 $ua->ssl_opts(verify_hostname => 0, SSL_cipher_list => 'AES128-SHA256', SSL_verify_mode => IO::Socket::SSL::SSL_VERIFY_NONE);
-$ua->default_header('Authorization' => "Basic YWRtaW46Y2hhbmdlbWU=");
+$ua->default_header('Authorization' => "Basic cm9vdDpjaGFuZ2VtZTM=");
 
 my $response = $ua->get('https://127.0.0.1:8089/services');
 
